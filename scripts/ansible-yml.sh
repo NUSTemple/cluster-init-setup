f020c252a32e6586cfea23ac37504440324e99f3
Tan Peng <tanpeng8847@gmail.com>
2022-03-27 00:35:02 +0800

    add copy config file to local yaml

--- scripts/ansible-yml.sh
+++ scripts/ansible-yml.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 file=$1
 echo "Use $file to complete the task"
+cd /home/pengtan/Projects/ansible/cluster-init-setup/scripts
 ansible-playbook -i ../hosts ../roles/common/tasks/k8s/$file*.yml -k -K
